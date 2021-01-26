56ad86ec638fdba719103b0831d15deddb3a5e43
PavelDubrov <dub1rov@yandex.ru>
2021-01-26 18:33:39 +0300

    homework-ver-1.0

--- src/main/java/ru/hh/school/entity/Area.java
+++ src/main/java/ru/hh/school/entity/Area.java
@@ -1,10 +1,21 @@
 package ru.hh.school.entity;
 
+import javax.persistence.*;
+
 //TODO: оформите entity
+
+@Entity
+@Table(name = "area")
 public class Area {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "area_id")
   private Integer id;
 
   private String name;
+
+  public Area() {}
 
   public String getName() {
     return name;
